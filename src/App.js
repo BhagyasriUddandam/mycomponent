84c92a9335073bc6b036640c37ef001c53894764
Bhagyasri Uddandam <bhagyasrirama@gmail.com>
2023-05-22 22:05:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,253 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+const MyComponent = () => {
+  const styles = {
+    container: {
+      position: "absolute",
+      width: "1440px",
+      height: "743px",
+      left: "0px",
+      top: "0px",
+      background: "#B8DDE3",
+    },
+    rectangle1271: {
+      position: "absolute",
+      width: "577.69px",
+      height: "468.45px",
+      left: "-519px",
+      top: "274.05px",
+      background: "#70C7C6",
+      transform: "matrix(1, -0.02, 0.02, 1, 0, 0)",
+    },
+    vector1: {
+      position: "absolute",
+      width: "577.69px",
+      height: "468.45px",
+      left: "1128px",
+      top: "150.62px",
+      background: "#DD246E",
+      transform: "matrix(1, -0.02, 0.02, 1, 0, 0)",
+    },
+    logoSpace: {
+      position: "absolute",
+      width: "230px",
+      height: "50px",
+      left: "136px",
+      top: "58px",
+      fontFamily: "Rubik",
+      fontStyle: "normal",
+      fontWeight: "700",
+      fontSize: "40px",
+      lineHeight: "50px",
+      textTransform: "capitalize",
+      color: "#010101",
+    },
+    text1: {
+      position: "absolute",
+      width: "526px",
+      height: "225px",
+      left: "137px",
+      top: "260px",
+      fontFamily: "Rubik",
+      fontStyle: "normal",
+      fontWeight: "700",
+      fontSize: "60px",
+      lineHeight: "75px",
+      textTransform: "capitalize",
+      color: "#0E1133",
+    },
+    col6: {
+      color: "#0E1133",
+    },
+    text2: {
+      position: "absolute",
+      width: "511px",
+      height: "26px",
+      left: "137px",
+      top: "514px",
+      fontFamily: "Red Hat Display",
+      fontStyle: "normal",
+      fontWeight: "400",
+      fontSize: "20px",
+      lineHeight: "26px",
+      color: "#010101",
+    },
+    getStarted: {
+      position: "absolute",
+      width: "98px",
+      height: "24px",
+      left: "46px",
+      top: "13px",
+      fontFamily: "Red Hat Display",
+      fontStyle: "normal",
+      fontWeight: "500",
+      fontSize: "18px",
+      lineHeight: "24px",
+      textAlign: "right",
+      color: "#FFFFFF",
+    },
+    home: {
+      position: "absolute",
+      width: "46px",
+      height: "23px",
+      left: "657px",
+      top: "70px",
+      fontFamily: "Source Sans Pro",
+      fontStyle: "normal",
+      fontWeight: "400",
+      fontSize: "18px",
+      lineHeight: "23px",
+      color: "#0E1133",
+    },
+    aboutUs: {
+      position: "absolute",
+      width: "67px",
+      height: "23px",
+      left: "738px",
+      top: "70px",
+      fontFamily: "Source Sans Pro",
+      fontStyle: "normal",
+      fontWeight: "400",
+      fontSize: "18px",
+      lineHeight: "23px",
+      color: "#0E1133",
+    },
+    courses: {
+      position: "absolute",
+      width: "61px",
+      height: "23px",
+      left: "840px",
+      top: "70px",
+      fontFamily: "Source Sans Pro",
+      fontStyle: "normal",
+      fontWeight: "400",
+      fontSize: "18px",
+      lineHeight: "23px",
+      color: "#0E1133",
+    },
+    rectangle2827: {
+      position: "absolute",
+      width: "557px",
+      height: "379px",
+      left: "748px",
+      top: "260px",
+      background: "#FFFFFF",
+      borderRadius: "15px",
+    },
+    rectangle2829: {
+      boxSizing: "border-box",
+      position: "absolute",
+      width: "236px",
+      height: "40px",
+      left: "786px",
+      top: "290px",
+      background: "#FFFFFF",
+      border: "0.5px solid rgba(9, 48, 81, 0.4)",
+      borderRadius: "5px",
+    },
+    phoneNumber: {
+      position: "absolute",
+      width: "96px",
+      height: "16px",
+      left: "1084px",
+      top: "302px",
+      fontFamily: "Roboto",
+      fontStyle: "normal",
+      fontWeight: "400",
+      fontSize: "14px",
+      lineHeight: "16px",
+      letterSpacing: "0.0125em",
+      color: "#777777",
+    },
+    rectangle2828: {
+      boxSizing: "border-box",
+      position: "absolute",
+      width: "486px",
+      height: "45px",
+      left: "786px",
+      top: "350px",
+      background: "#FFFFFF",
+      border: "0.5px solid rgba(9, 48, 81, 0.4)",
+      borderRadius: "5px",
+    },
+    rectangle2831: {
+      boxSizing: "border-box",
+      position: "absolute",
+      width: "486px",
+      height: "99px",
+      left: "786px",
+      top: "415px",
+      background: "#FFFFFF",
+      border: "0.5px solid rgba(9, 48, 81, 0.4)",
+      borderRadius: "5px",
+    },
+    button: {
+      position: "absolute",
+      width: "132px",
+      height: "20px",
+      left: "947px",
+      top: "561px",
+      fontFamily: "Red Hat Display",
+      fontStyle: "normal",
+      fontWeight: "600",
+      fontSize: "20px",
+      lineHeight: "20px",
+      letterSpacing: "1px",
+      textTransform: "capitalize",
+      color: "#FFFFFF",
+    },
+    mockTests: {
+      position: "absolute",
+      width: "82px",
+      height: "23px",
+      left: "936px",
+      top: "70px",
+      fontFamily: "Source Sans Pro",
+      fontStyle: "normal",
+      fontWeight: "400",
+      fontSize: "18px",
+      lineHeight: "23px",
+      color: "#0E1133",
+    },
+    contactUs: {
+      position: "absolute",
+      width: "92px",
+      height: "24px",
+      left: "41.5px",
+      top: "13px",
+      fontFamily: "Red Hat Display",
+      fontStyle: "normal",
+      fontWeight: "500",
+      fontSize: "18px",
+      lineHeight: "24px",
+      textAlign: "center",
+      color: "#FFFFFF",
+    },
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={styles.container}>
+      <div style={styles.rectangle1271}></div>
+      <div style={styles.vector1}></div>
+      <div style={styles.logoSpace}>Logo Space</div>
+      <div style={styles.text1}>Get the best Laundry Service at your doorstep</div>
+      <div style={styles.text2}>
+        Book laundry service with our app in just a few &amp; easy steps
+      </div>
+      <div style={styles.getStarted}>Get Started</div>
+      <div style={styles.home}>Home</div>
+      <div style={styles.aboutUs}>About Us</div>
+      <div style={styles.courses}>Courses</div>
+      <div style={styles.rectangle2827}></div>
+      <div style={styles.rectangle2829}></div>
+      <div style={styles.phoneNumber}>Phone Number</div>
+      <div style={styles.rectangle2828}></div>
+      <div style={styles.rectangle2831}></div>
+      <div style={styles.button}>Button</div>
+      <div style={styles.mockTests}>Mock Tests</div>
+      <div style={styles.contactUs}>Contact Us</div>
     </div>
   );
-}
+};
 
-export default App;
+export default MyComponent;
